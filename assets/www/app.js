bfea4b2fa75cb14f2df78b9335c3565134ecbbcf
ovilia <zwl.sjtu@gmail.com>
2012-08-28 11:10:33 +0800

    Level structure. Sea drawing.

--- assets/www/app.js
+++ assets/www/app.js
@@ -7,6 +7,12 @@ var frameRate = 25; // 25 frames each second
 
 fishArray = new Array();
 
+// height of sky
+skyHeight = 0;
+// height of shallow sea
+shallowHeight = 20;
+// height of deep sea is the rest
+
 $(document).ready(function() {
 	width = window.innerWidth;
 	height = window.innerHeight;
@@ -16,24 +22,19 @@ $(document).ready(function() {
 	canvas = $("#canvas").get(0);
 	
 	if(canvas) {
-		var query = window.location.search.substring(1);
-		console.log(query);
-		if (query.substring(0, 6) == "level=") {
-			var jsFile = "levels/" + query.substring(6) + ".js";
-			
-			// include js file according to different levels
-			var dynamicInclude = document.createElement("script");
-			dynamicInclude.src = jsFile;
-			dynamicInclude.type = "text/javascript";
-			document.getElementsByTagName("head")[0].appendChild(dynamicInclude);
-
-			// start game
-			init();
-		}
+		// level information in cookie
+		var level = window.localStorage.getItem("level");
+		var script = document.createElement('script');
+		script.src = "levels/teach" + level + ".js";
+		script.type = 'text/javascript';
+		document.getElementsByTagName('head')[0].appendChild(script);
+		init();
 	}
 });
 
 function init() {
+	energy = new EnergyManager();
+
 	frameCnt = 0;
 	setInterval(draw, Math.ceil(1000 / frameRate));
 }
@@ -45,8 +46,20 @@ function draw() {
 
 	var ctx = canvas.getContext('2d');
 	
-	ctx.globalCompositeOperation = "destination-over";
 	ctx.clearRect(0, 0, width, height);
+	
+	// background of sky and sea
+	// shallow sea
+	ctx.fillStyle = "#8ED6FF";
+	ctx.fillRect(0, 0, width, shallowHeight);
+	// deep sea
+	ctx.fillStyle = "#5483ca";
+	ctx.fillRect(0, shallowHeight, width, height - shallowHeight);
+	
+	// energy ball
+	ctx.drawImage(energy.image, 
+		width - energy.image.width - 15, height - energy.image.height - 15,
+		energy.image.width, energy.image.height);
 	
 	var len = fishArray.length;
 	for (var i = 0; i < len; ++i) {
@@ -78,4 +91,8 @@ function SmallFish(size) {
 	this.speed = 20 / (size + 5);
 }
 
+function EnergyManager() {
+	this.image = new Image();
+	this.image.src = "images/energy.png";
+}
 
--- assets/www/levels/teach1.js
+++ assets/www/levels/teach1.js
@@ -3,7 +3,7 @@ function fishManager() {
 	if (frameCnt % (4 * frameRate) == 0) { // add fish each 4 sec
 		var id = Math.floor(Math.random() * 7) + 1;
 		var fish = new SmallFish(id);
-		
+
 		fishArray.push(fish);
 	}
 }
