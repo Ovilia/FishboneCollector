bff535fcd68dc350cd7d1fa76a6055c443704636
ovilia <zwl.sjtu@gmail.com>
2012-08-28 13:47:01 +0800

    Bubble

--- assets/www/app.js
+++ assets/www/app.js
@@ -1,28 +1,15 @@
-var canvas;
-
-var width = 0;
-var height = 0;
-
-var frameRate = 25; // 25 frames each second
-
-fishArray = new Array();
-
-// height of sky
-skyHeight = 0;
-// height of shallow sea
-shallowHeight = 20;
-// height of deep sea is the rest
-
 $(document).ready(function() {
 	width = window.innerWidth;
 	height = window.innerHeight;
  
 	$("#canvas").attr("width", width);
 	$("#canvas").attr("height", height);
-	canvas = $("#canvas").get(0);
+	var canvas = $("#canvas").get(0);
 	
 	if(canvas) {
-		// level information in cookie
+		ctx = canvas.getContext('2d');
+		
+		// level information in local storage
 		var level = window.localStorage.getItem("level");
 		var script = document.createElement('script');
 		script.src = "levels/teach" + level + ".js";
@@ -33,18 +20,51 @@ $(document).ready(function() {
 });
 
 function init() {
+	fishArray = new Array();
+	
+	// height of sky
+	skyHeight = 0;
+	// height of shallow sea
+	shallowHeight = 50;
+	// height of deep sea is the rest
+	
 	energy = new EnergyManager();
-
+	bubble = new BubbleManager();
+	
+	frameRate = 25; // 25 frames each second
 	frameCnt = 0;
 	setInterval(draw, Math.ceil(1000 / frameRate));
+	
+	document.addEventListener("deviceready", onDeviceReady, false);
+	
+	isPaused = false;
+	isMousePressed = false;
+	$('#canvas').mousedown(function(e) {
+		isMousePressed = true;
+		bubble.addBubble(e.pageX, e.pageY);
+	});
+	$('#canvas').mouseup(function(e) {
+		isMousePressed = false;
+		var len = bubble.bubbleArray.length;
+		if (len > 0) {
+			bubble.bubbleArray[len - 1]["active"] = false;
+		}
+	});
+	$('#canvas').mousemove(function(e) {
+		if (isMousePressed) {
+			var len = bubble.bubbleArray.length;
+			if (len > 0 && bubble.bubbleArray[len - 1]["active"] === true) {
+				bubble.bubbleArray[len - 1]["x"] = e.pageX;
+				bubble.bubbleArray[len - 1]["y"] = e.pageY;
+			}
+		}
+	});
 }
 
 function draw() {
 	++frameCnt;
 	// check if to add or delete fish
 	fishManager();
-
-	var ctx = canvas.getContext('2d');
 	
 	ctx.clearRect(0, 0, width, height);
 	
@@ -60,8 +80,29 @@ function draw() {
 	ctx.drawImage(energy.image, 
 		width - energy.image.width - 15, height - energy.image.height - 15,
 		energy.image.width, energy.image.height);
+		
+	// bubble
+	var len = bubble.bubbleArray.length;
+	for (var i = 0; i < len; ++i) {
+		var bWidth = bubble.bubbleArray[i]["energy"] * bubble.image.width;
+		var bHeight = bubble.bubbleArray[i]["energy"] * bubble.image.height;
+		ctx.drawImage(bubble.image,
+			bubble.bubbleArray[i]["x"] - bWidth / 2, bubble.bubbleArray[i]["y"] - bHeight / 2,
+			bWidth, bHeight);
+			
+		if (bubble.bubbleArray[i]["active"] === false) {
+			// float up
+			bubble.bubbleArray[i]["x"] += Math.random() * 10 - 5;
+			bubble.bubbleArray[i]["y"] -= bubble.bubbleArray[i]["energy"] * 20;
+		}
+	}
+	// enlarge if mouse is pressed
+	if (isMousePressed) {
+		bubble.enlargeBubble();
+	}
 	
-	var len = fishArray.length;
+	// fish
+	len = fishArray.length;
 	for (var i = 0; i < len; ++i) {
 		if (fishArray[i]) {
 			ctx.drawImage(fishArray[i].image, 
@@ -78,6 +119,19 @@ function draw() {
 	}
 }
 
+function onDeviceReady() {
+	document.addEventListener("pause", onPause, false);
+	document.addEventListener("resume", onResume, false);
+}
+
+function onPause() {
+	isPaused = true;
+}
+
+function onResume() {
+	isPaused = false;
+}
+
 /*
  * Definition of classes
  */
@@ -89,6 +143,7 @@ function SmallFish(size) {
 	this.top = Math.ceil(Math.random() * (height - this.image.height));
 	this.size = size;
 	this.speed = 20 / (size + 5);
+	
 }
 
 function EnergyManager() {
@@ -96,3 +151,31 @@ function EnergyManager() {
 	this.image.src = "images/energy.png";
 }
 
+function BubbleManager() {
+	this.image = new Image();
+	this.image.src = "images/bubble.png";
+	
+	this.bubbleArray = new Array();
+	
+	this.addBubble = function(x, y) {
+		var bubble = {
+			"x": x,
+			"y": y,
+			"energy": 1.0 / 6.0, 
+			"active": true // those active ones can be enlarged
+		};
+		this.bubbleArray.push(bubble);
+	};
+	
+	this.enlargeBubble = function() {
+		// only the last one bubble can be active in bubbleArray
+		var len = this.bubbleArray.length;
+		if (this.bubbleArray[len - 1]["active"] === true) {
+			this.bubbleArray[len - 1]["energy"] += 1.0 / 32.0;
+		}
+		if (this.bubbleArray[len - 1]["energy"] >= 1.0) {
+			// energy is full
+			this.bubbleArray[len - 1]["energy"] = 1.0;
+		}
+	};
+}
